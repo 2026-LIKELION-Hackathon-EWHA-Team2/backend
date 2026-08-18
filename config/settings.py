3b5798aacb6660effe08bc2dc6bc1b57813dfe1a
HaenaneaH <tnwn0225@gmail.com>
2026-08-18 11:55:26 +0900

    fix: CORS 미들웨어 실행 순서 조정

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,8 @@ AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
